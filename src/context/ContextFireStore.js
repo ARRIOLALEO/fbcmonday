df396370458779708e98b1c3a40cc3d33430cc3c
arriolaleo <riblarsv@gmail.com>
2022-04-05 15:18:01 +0600

    added to the upload image

--- src/context/ContextFireStore.js
+++ src/context/ContextFireStore.js
@@ -1,43 +1,48 @@
 import { createContext, useEffect, useState } from "react";
-import { collection, addDoc, getDocs, deleteDoc, doc, onSnapshot  } from "firebase/firestore";
-import {ref,uploadBytes,getDownloadURL} from 'firebase/storage'
+import { collection, addDoc, getDocs, deleteDoc, doc, onSnapshot } from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import * as firebaseApp from "../firebase/configFirebase";
 export const FirebaseContext = createContext([]);
 
 const FirebaseProvider = ({ children }) => {
   const refCollection = collection(firebaseApp.bd, "todos");
   const [allTodos, setAllTodos] = useState([]);
-
+  const [newImageUrl, setnewImageUrl] = useState("");
+  const [progressImageUpload, setProgressImageUpload] = useState(0);
 
   onSnapshot(doc(firebaseApp.bd, "todos", "todo"), async (doc) => {
     getAllTodos();
   });
 
-  const uploadImage = async(image) =>{
-    try{
-      const metadata = {
-        contentType: 'image/jpeg',
-      };
-    const referenceImage = await ref(firebaseApp.storage,`images/${image}`,metadata) 
-    const  imageToUpload = await uploadBytes(referenceImage,image)
-    const  uploadedImage = await imageToUpload
-    const url = await getDownloadURL(ref(firebaseApp.storage, `images/${image}`))
-    return url
-    }catch(err){
-      console.log(err.message)
-    }
-  }
+  const uploadImage = (image, todo) => {
+    console.log(image);
+    const referenceImage = ref(firebaseApp.storage, `images/${image.name}`);
+    const uploadImage = uploadBytesResumable(referenceImage, image);
+    uploadImage.on(
+      "state_changed",
+      (snapshot) => {
+        console.log(snapshot.bytesTransferred);
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(`the image is % ${progress}`);
+      },
+      (err) => {
+        console.log(err.message);
+      },
+      () =>
+        getDownloadURL(uploadImage.snapshot.ref).then((url) =>
+          addDoc(refCollection, { todo: todo, url: url })
+        )
+    );
+  };
   const deleteTodo = async (id) => {
     const todoSelected = await doc(firebaseApp.bd, "todos", id);
     const removed = await deleteDoc(todoSelected);
     console.log(removed);
   };
 
-  const addTodo = async (newTodo,image) => {
-    
+  const addTodo = async (newTodo, image) => {
     try {
-      const imageUrl = await uploadImage(image.name)
-      await addDoc(refCollection, { todo: newTodo,url:imageUrl });
+      const imageUrl = await uploadImage(image, newTodo);
     } catch (err) {
       console.log(err.message);
     }
@@ -57,7 +62,7 @@ const FirebaseProvider = ({ children }) => {
   const myApp = {
     addTodo: addTodo,
     allTodos: allTodos,
-    uploadImage:uploadImage,
+    uploadImage: uploadImage,
     deleteTodo: deleteTodo,
   };
   return <FirebaseContext.Provider value={myApp}>{children}</FirebaseContext.Provider>;
